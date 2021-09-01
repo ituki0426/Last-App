b83f8e34abbb3ea0aeb3524732c3882cfaec3ae8
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-09-01 15:10:32 +0900

    'main'

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,15 @@ const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('api', (req, res, next) => {
+
+app.get('/api', (req, res, next) => {
   res.set({ 'Access-Control-Allow-Origin': '*' }); // ここでヘッダーにアクセス許可の情報を追加
   res.json({ message: "Hello World!" });
 });
-
 app.get('*', (req, res, next) => {
   res.set({ 'Access-Control-Allow-Origin': '*' }); // ここでヘッダーにアクセス許可の情報を追加
   res.sendFile(path.join(__dirname,'../frontend/build/index.html'));
 });
-
 app.listen(port, () => {
   console.log(`listening on *:${port}`);
 })
